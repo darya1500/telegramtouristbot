d1ad522ca89fc0bfc66976aef8965b0805ee1c58
darya <daryatarasevich15@gmail.com>
2020-07-10 12:40:24 +0300

    first commit

---
+++ src/main/java/by/resliv/daryatarasevich/telegramtouristbot/TouristBot.java
@@ -0,0 +1,127 @@
+package by.resliv.daryatarasevich.telegramtouristbot;
+
+
+import org.telegram.telegrambots.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TouristBot extends TelegramLongPollingBot {
+
+    /**
+     * Метод для приема сообщений.
+     *
+     * @param update Содержит сообщение от пользователя.
+     */
+
+    @Override
+    public void onUpdateReceived(Update update) {
+//        String message = update.getMessage().getText();
+//        sendMsg(update.getMessage().getChatId().toString(), message);
+        if (update.hasMessage()) {
+            ThreadClass thread = new ThreadClass(update.getMessage());
+        } else if (update.hasCallbackQuery()) {
+            AnswerCallbackThread answerThread = new AnswerCallbackThread(update.getCallbackQuery());
+        }
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     *
+     * @param chatId id чата
+     * @param s      Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        setButtons(sendMessage);
+        setInline();
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            //todo log
+            // log.log(Level.SEVERE, "Exception: ", e.toString());
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     *
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return "TelegramTouristBot";
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     *
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return "1379659363:AAFef_wY_4KOJx2iP0b4wgdnTlkUYYrKymk";
+    }
+
+    public synchronized void setButtons(SendMessage sendMessage) {
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add(new KeyboardButton("Привет"));
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add(new KeyboardButton("Помощь"));
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    private void setInline() {
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<List<InlineKeyboardButton>>();
+        List<InlineKeyboardButton> buttons1 = new ArrayList<InlineKeyboardButton>();
+        buttons1.add(new InlineKeyboardButton().setText("Кнопка").setCallbackData(String.valueOf(17)));
+        buttons.add(buttons1);
+
+        InlineKeyboardMarkup markupKeyboard = new InlineKeyboardMarkup();
+        markupKeyboard.setKeyboard(buttons);
+    }
+
+    public synchronized void answerCallbackQuery(String callbackId, String message) {
+        AnswerCallbackQuery answer = new AnswerCallbackQuery();
+        answer.setCallbackQueryId(callbackId);
+        answer.setText(message);
+        answer.setShowAlert(true);
+        try {
+            answerCallbackQuery(answer);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
